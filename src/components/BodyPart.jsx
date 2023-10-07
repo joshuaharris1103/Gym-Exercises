79156c3d8fb8e6e701baa4eaafa460bd136ebf19
jrh <joshuaharris1103@gmail.com>
2023-10-07 19:19:35 -0400

    finishing up exercise component

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -4,6 +4,7 @@ import { Stack, Typography } from '@mui/material'
 import Icon from '../assets/icons/gym.png'
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+  
   return (
     <Stack
       type='button'
