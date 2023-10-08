7848f2dba7211303592f4a3f79e711882d907db2
jrh <joshuaharris1103@gmail.com>
2023-10-07 20:58:20 -0400

    working gifs on exercise cards

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -15,7 +15,7 @@ const ExerciseCard = ( {exercise} ) => {
             <Button sx={{ ml:'21px', color: '#fff', background: '#fcc757',
             fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'
         }}>
-            {exercise.bodyPart}
+            {exercise.target}
             </Button>
         </Stack>
         <Typography ml='21px' color='#000' fontWeight='bold' mt='11px' pb='10px' textTransform='capitalize'  fontSize='24px'>
